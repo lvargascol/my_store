512b292ec705e4c8574dc7260f21079e2f61b6bb
lvargascol <lvargascol@gmail.com>
2022-07-07 22:11:45 -0400

    migracion create order table corregida

--- db/migrations/20220702181733-Create Orders.js
+++ db/migrations/20220702181733-Create Orders.js
@@ -1,10 +1,41 @@
 'use strict';
 
-const { ORDER_TABLE, OrderSchema } = require("../models/orderModel");
+const { ORDER_TABLE } = require("../models/orderModel");
+const { DataTypes, Sequelize } = require('sequelize');
+const { CUSTOMER_TABLE } = require('../models/customerModel');
 
 module.exports = {
   async up (queryInterface) {
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      paid: {
+        allowNull: false,
+        type: DataTypes.BOOLEAN,
+        default: false
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        reference: {
+          model: CUSTOMER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+        createdAt: {
+          allowNull: false,
+          type: DataTypes.DATE,
+          field: 'create_at',
+          defaultValue: Sequelize.NOW
+        },
+    });
   },
 
   async down (queryInterface) {
